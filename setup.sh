4bcdb2ba89befb93529f2ffaee0e8bf052616de4
juanpvh <juancm_pvh@gmail.com>
2019-09-09 18:05:52 -0300

    up6

--- setup.sh
+++ setup.sh
@@ -11,6 +11,9 @@ echo "INSTALANDO WO-CLI.."
     [ -e /usr/local/bin/wo-cli ] && echo "wo-cli Existe ⚡️" || wget -O /usr/local/bin/wo-cli https://raw.githubusercontent.com/juanpvh/wo-cli/master/wo-cli.sh
  chmod +x /usr/local/bin/wo-cli
 
+echo -ne "Digite o Nome da Pasta Onde ficara os BackUps: " ; read DIR 
+sed -i "s/BACKUPS=BK/BACKUPS=$DIR/" /usr/local/bin/wo-cli
+
 echo "Rclone e WO-CLI instalados"
 echo
 echo -ne "Configurar o Rclone️ para google drive? [y/n] [y]: "; read -i n INS1
